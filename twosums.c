652ef56585b2a8054ddd691f8f0c0f2f6a1195b5
Alibek <alibekberik13@gmail.com>
2025-10-02 19:27:22 +0800

    Add Two Sum solution in C

---
+++ twosums.c
@@ -0,0 +1,27 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    for(int i=0;i<numsSize;i++){
+        for(int j=i+1;j<numsSize;j++){
+            if(nums[i]+nums[j] == target){
+                *returnSize = 2;
+                int *result = malloc(2*sizeof(int));
+                result[0]=i;
+                result[1]=j;
+                return result;
+
+
+            
+
+
+
+            }
+        }
+
+    }
+    *returnSize = 0;  // no solution (safety, though problem says always one)
+    return 0;
+
+    
+}
